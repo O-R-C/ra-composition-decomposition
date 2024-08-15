6f028fe6e0995d19f64629771fbd9b3bb27e2260
Andrey Semenov <semenov.orc@gmail.com>
2024-08-15 08:53:33 +0300

    add description Search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,25 @@
 import styles from './Search.module.css'
 
+/**
+ * A React functional component that renders a search bar with an input field and a button.
+ *
+ * @return {JSX.Element} The rendered search bar element.
+ */
 const Search = () => {
   return (
-    <div className={styles.search}>
-      <div className={styles.icon}>🔎</div>
-      <input
-        className={styles.input}
-        type='text'
-        placeholder='Search...'
-      />
-      <button className={styles.button}>Найти</button>
+    <div className={styles.searchContainer}>
+      <div className={styles.search}>
+        <div className={styles.icon}>🔎</div>
+        <input
+          className={styles.input}
+          type='text'
+          placeholder='Search...'
+        />
+        <button className={styles.button}>Найти</button>
+      </div>
+      <div className={styles.example}>
+        Найдётся всё. Например, <a href='#'>фаза луны сегодня</a>
+      </div>
     </div>
   )
 }
