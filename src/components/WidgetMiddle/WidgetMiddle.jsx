d6a7a86909f1a2891d8e3f99af98b02a40bff795
Andrey Semenov <semenov.orc@gmail.com>
2024-08-15 09:48:26 +0300

    add WidgetMiddle

--- src/components/WidgetMiddle/WidgetMiddle.jsx
+++ src/components/WidgetMiddle/WidgetMiddle.jsx
@@ -5,6 +5,14 @@ import Banner from '../Banner/Banner'
 
 import styles from './WidgetMiddle.module.css'
 
+/**
+ * A React component that renders the middle widget.
+ *
+ * @param {Object} props - The component's props.
+ * @param {Array} props.appLinks - An array of application links.
+ * @param {Object} props.bannerData - An object containing banner data.
+ * @return {JSX.Element} A JSX element representing the middle widget.
+ */
 const WidgetMiddle = ({ appLinks, bannerData }) => {
   return (
     <div className={styles.widgetMiddle}>
